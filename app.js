b7db1afe86b7c408f5cbd824e635a538ee2142fa
adamcockell1 <adam.cockell1@gmail.com>
2023-07-13 16:11:58 -0400

    🎨Changed double to single quotes (non-html)

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-"use strict";
+'use strict';
 
-const navBar = document.querySelector(".navBar");
-const navButtons = document.getElementsByClassName("navButton");
-const icon = document.querySelector(".icon");
-const main = document.querySelector(".main");
+const navBar = document.querySelector('.navBar');
+const navButtons = document.getElementsByClassName('navButton');
+const icon = document.querySelector('.icon');
+const main = document.querySelector('.main');
 
 /* Theme and color button functions */
 
 let filters = [];
 
 function theme() {
-    if (filters.includes("invert(1)")) {
+    if (filters.includes('invert(1)')) {
         // Find and remove invert from the list of filters and update button text
-        filters.splice(filters.indexOf("invert(1)"), 1);
-        document.querySelector(".themeBtn").textContent = "DARK MODE";
+        filters.splice(filters.indexOf('invert(1)'), 1);
+        document.querySelector('.themeBtn').textContent = 'DARK MODE';
     } else {
         // Add invert to the list of filters and update button text
-        filters.push("invert(1)");
-        document.querySelector(".themeBtn").textContent = "LIGHT MODE";
+        filters.push('invert(1)');
+        document.querySelector('.themeBtn').textContent = 'LIGHT MODE';
     }
     // Overwrite currently applied filters with what is stored in the filters list
-    document.documentElement.style.filter = filters.join(" ");
+    document.documentElement.style.filter = filters.join(' ');
 }
 
 function color() {
-    if (filters.includes("grayscale(1)")) {
+    if (filters.includes('grayscale(1)')) {
         // Find and remove grayscale from the list of filters and update button text
-        filters.splice(filters.indexOf("grayscale(1)"), 1);
-        document.querySelector(".colorBtn").textContent = "FULL COLOR";
+        filters.splice(filters.indexOf('grayscale(1)'), 1);
+        document.querySelector('.colorBtn').textContent = 'FULL COLOR';
     } else {
         // Add grayscale to the list of filters and update button text
-        filters.push("grayscale(1)");
-        document.querySelector(".colorBtn").textContent = "GRAYSCALE";
+        filters.push('grayscale(1)');
+        document.querySelector('.colorBtn').textContent = 'GRAYSCALE';
     }
     // Overwrite currently applied filters with what is stored in the filters list
-    document.documentElement.style.filter = filters.join(" ");
+    document.documentElement.style.filter = filters.join(' ');
 }
 
 /* Logo fade in on page load */
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
     // Set icon origin point
-    icon.style.transition = "1s";
-    icon.style.left = (window.innerWidth - icon.width) / 2 + "px";
-    icon.style.top = (window.innerHeight - icon.height) / 2 + "px";
-    icon.style.opacity = "1";
+    icon.style.transition = '1s';
+    icon.style.left = (window.innerWidth - icon.width) / 2 + 'px';
+    icon.style.top = (window.innerHeight - icon.height) / 2 + 'px';
+    icon.style.opacity = '1';
 });
 
 /* Navbar reveal and image scale/transform on scoll */
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
     let scale = 100 - window.scrollY / 5.75;
     let left =
         (window.innerWidth - icon.width) / 2 -
@@ -63,18 +63,18 @@ window.addEventListener("scroll", () => {
         for (let i = 0; i < navButtons.length; i++) {
             navButtons[i].disabled = true;
         }
-        navBar.style.opacity = "0";
+        navBar.style.opacity = '0';
         // Disable transition for snappier effect
-        icon.style.transition = "none";
+        icon.style.transition = 'none';
         // Scale and translate icon
-        icon.style.transform = "scale(" + scale + "%)";
-        icon.style.left = left + "px";
-        icon.style.top = top + "px";
+        icon.style.transform = 'scale(' + scale + '%)';
+        icon.style.left = left + 'px';
+        icon.style.top = top + 'px';
     } else {
         // Re-enable transition for color/theme changes
-        icon.style.transition = "0.5s";
+        icon.style.transition = '0.5s';
         // Re-enable the navbar after the icon transform effect finishes
-        navBar.style.opacity = "1";
+        navBar.style.opacity = '1';
         for (let i = 0; i < navButtons.length; i++) {
             navButtons[i].disabled = false;
         }
